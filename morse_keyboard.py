6dbf2b643019e3556d8b800d9611bccd2607ed1c
Anne <52967012+mocchapi@users.noreply.github.com>
2019-11-12 17:35:14 +0100

    Add files via upload

---
+++ morse_keyboard.py
@@ -0,0 +1,43 @@
+from msvcrt import getch
+import winsound
+import time
+global alphabet
+alphabet = [10111, 111010101, 11101011101, 1110101, 1, 101011101, 111011101, 1010101, 101, 1011101110111, 111010111, 101110101, 1110111, 11101, 11101110111, 10111011101, 1110111010111, 1011101, 10101, 111, 1010111, 101010111, 101110111, 11101010111, 1110101110111, 11101110101]
+
+def beeper(int_in, unit=100, frequency=600, space=False):
+	if space == True:
+		for idx,_ in enumerate(range(7)):
+			print(round(idx/7*100,2),"%")
+			time.sleep(unit*7/1000)
+		print("100.0 %")
+		print("---")
+		return
+	bits = list(filter(None,str(int_in).replace("0","s0s").split("s")))
+	for idx, x in enumerate(bits):
+		print(round(idx/len(bits)*100,2),"%")
+		try:
+			if x == "0":
+				time.sleep(unit/1000)
+			elif len(x) == 1:
+				winsound.Beep(frequency,unit)
+			else:
+				winsound.Beep(frequency,unit*3)
+		except BaseException as e:
+			print(e)
+			return e
+	print("100.0 %")
+	print("---")
+	time.sleep(unit/1000*3)
+
+while True:
+	key = ord(getch())
+	try:
+		if 96<key<123:
+			key = key-97
+			beeper(alphabet[key])
+		elif key == 32:
+			beeper(0,space=True)
+		else:
+			print("not in range")
+	except Exception as e:
+		raise e
